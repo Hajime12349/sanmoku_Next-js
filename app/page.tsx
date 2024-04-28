4e570267b0b642c5b3125df6fe210855d7f04436
Hajime12349 <qqwwkami@gmail.com>
2024-04-28 20:43:49 +0900

    matlix button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
+const Square =()=> <button className={styles.square}>1</button>
+
+
+import styles from './style.module.css'
 export default function Home() {
   return (
     <>
+      <div className={styles.boardrow}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className={styles.boardrow}>
+        <Square />
+        <Square />
+        <Square />  
+      </div>
+      <div className={styles.boardrow}>
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
+    
   );
 }
